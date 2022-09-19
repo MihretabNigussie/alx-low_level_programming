79fd7bfdb95624895a718c09046bab0a3d3cd8e8
MihretabNigussie <meresenior14232314@gmail.com>
2022-09-19 04:37:07 -0700

    This is it

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,9 +1,11 @@
 #include <stdio.h>
+
 /**
  * main - prints to string
  * Description: Prints "and that piece of art is useful.." without puts
  * Return: 1
  */
+
 int main(void)
 {
 	char *s = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
@@ -11,6 +13,11 @@ int main(void)
 	long fd = 1;
 	long syscall = 1;
 	long ret = 0;
-	__asm__ ("syscall": "=a" (ret): "a" (syscall),"D" (fd),"S" (s),"d" (l));
+	__asm__ ("syscall"
+	: "=a" (ret)
+	: "a" (syscall),
+	"D" (fd),
+	"S" (s),
+	"d" (l));
 	return (1);
 }
