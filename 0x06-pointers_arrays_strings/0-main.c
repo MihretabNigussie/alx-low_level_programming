b1b4682c40715f6f0968b31645c8f8651cf1f508
MihretabNigussie <meresenior14232314@gmail.com>
2022-09-21 04:53:17 -0700

    This is it

--- 0x06-pointers_arrays_strings/0-main.c
+++ 0x06-pointers_arrays_strings/0-main.c
@@ -2,18 +2,22 @@
 #include <stdio.h>
 
 /**
- * main - checks the code
+ * main - checks the code for ALX School students.
  *
  * Return: Always 0.
  */
 
 int main(void)
 {
-	char c;
+	char s1[98] = "Hello ";
+	char s2[] = "World!\n";
+	char *ptr;
 
-	c = 'A';
-	printf("%c: %d\n", c, _isupper(c));
-	c = 'a';
-	printf("%c: %d\n", c, _isupper(c));
+	printf("%s\n", s1);
+	printf("%s", s2);
+	ptr = _strcat(s1, s2);
+	printf("%s", s1);
+	printf("%s", s2);
+	printf("%s", ptr);
 	return (0);
 }
